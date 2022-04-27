7d1acab8b874a3cbfcecafb82e4c62b827605553
Zanete Dijeva <zanete.dijeva@gmail.com>
2022-04-27 22:38:40 +0100

    Day 1

---
+++ 1.a.rkt
@@ -0,0 +1,29 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname 1.a) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;(+ 3 4)
+;(+ 3 (* 2 3))
+;(/ 12 (* 2 3))
+;
+;(sqr 3)
+;
+;(sqrt 16)
+
+;(/ (+ 4 6.2 -12)3)
+
+;( + 2 (* 3 4) (- (+ 1 2) 3))
+;( + 2 12 (- (+ 1 2) 3))
+;( + 2 12 (- 3 3))
+;( + 2 12 0)
+
+;"apple"
+;(string-append "Ada" " " "Lovelace")
+
+;(string-length "apple")
+;
+;(substring "Caribou" 2 4)
+;           "0123456789"
+;(substring "0123456789" 2 4)
+
+;(substring "Caribou" 0 3)
+
